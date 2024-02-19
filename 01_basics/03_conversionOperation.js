6231c5fd669835e03f77563651ae07aec4e6c61d
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-02-19 14:25:53 +0000

    datatypes conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,26 @@
-let score = "sourav"
+let score = "true"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
 
 
 // "33" => 33
 // "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "g"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "sourav" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
