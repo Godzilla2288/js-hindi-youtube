88629c34c5f65e85e1a72b04aa6e76792538691e
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-02-22 07:46:41 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// primitive
+
+// 7 typpes : String, Number, Boolean, Null, Undefined, Symbol,
+// BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+//console.log(typeof userEmail);
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 34465682316764676n
+
+
+// Reference ( Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name : "sourav",
+    age : 24,
+}
+
+const myFunction = function () {
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
