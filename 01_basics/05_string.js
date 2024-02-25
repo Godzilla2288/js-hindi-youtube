75fa426ae4459688d4d99fb00cdc66a87d28fb0c
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-02-25 05:43:30 +0000

    string ends

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc')
+const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName[0]);
 // console.log(name[0]);
@@ -27,6 +27,14 @@ console.log(anotherString);
 const newStringOne = "    hitesh.    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace("%20","-"));
+
+console.log(url.includes("hitesh"));
+
+console.log(gameName.split('-'));
 
 
 
