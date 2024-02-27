a1a4dedc653197f2e120ede7d84810e827d3b6a8
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-02-27 11:03:22 +0000

    number maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,24 +1,29 @@
 const score = 400
-console.log(score);
+//console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+//console.log(balance);
 
-console.log(balance.toString().length);
+//console.log(balance.toString().length);
 //console.log(typeof balance);
-console.log(balance.toFixed(2));
+//console.log(balance.toFixed(2));
 
 const otherNumber = 123.8966
 
-console.log(otherNumber.toPrecision(4));
+//console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));
+//console.log(hundreds.toLocaleString('en-IN'));
 // [ 'en - IN' ] converts to Indian Counting standards
 
 // +++++++++++++++++ Maths ++++++++++++++++
 
-
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.00000001));
+console.log(Math.floor(4.99999999));
+console.log(Math.min(4, 3, 6, 8));
 
 
 
