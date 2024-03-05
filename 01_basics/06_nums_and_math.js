0286873c1db80b04309ba239e26fe2ac699a3d1e
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-03-05 07:39:22 +0000

    math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -18,16 +18,20 @@ const hundreds = 1000000
 
 // +++++++++++++++++ Maths ++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.5));
-console.log(Math.ceil(4.00000001));
-console.log(Math.floor(4.99999999));
-console.log(Math.min(4, 3, 6, 8));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.00000001));
+ //console.log(Math.floor(4.99999999));
+ // Lowest Value
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
 
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
 
+const min = 1
+const max = 6
 
-
-
-
-
+console.log(Math.floor(Math.random()*(max-min+1)) + min);
