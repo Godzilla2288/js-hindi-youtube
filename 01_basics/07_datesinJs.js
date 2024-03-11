dee2eea06749fdc8f8f6eb42da1fda1c594fb2e8
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-03-11 10:26:40 +0000

    timesssk

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -18,11 +18,29 @@ let myCreatedDate = new Date("01-14-2023")    // Same Answer
     // Indian type Date Time does not work.
 // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now()  // Time in MILLISECONDS
+let myTimeStamp = Date.now()  // Time in MILLISECONDS [Current Time after 1970s]
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime()); // The exact time when the Variable was created
 
-console.log(Math.floor(Date.now()/1000)); // Converts Milliseconds to Seconds
+// console.log(Math.floor(Date.now()/1000)); // Converts Milliseconds to Seconds
         // Math.floor to omit the Decimal value.
 
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());  // Month starts from zero [0]. | 2 [ MARCH ] 
+console.log(newDate.getMonth() + 1);  // Add + 1 to avoid USER confusion.
+
+console.log(newDate.getDay());  //  Output = 3[ WEDNESDAY ]
+          // Day counting starts from MONDAY.
+
+// console.log(`${newDate.getDay()} and the time is ...`);  // String Interpolation.
+
+console.log(newDate.toLocaleString( 'default', {
+weekday: "long",
+month : "long",
+day : "numeric",
+minute : "2-digit",
+hour12 :             // Doubt????
+
+}));
---
+++ 02_basics/01_array.js
@@ -0,0 +1,10 @@
+// array
+
+// () = Parenthesis
+// {} = Braces/Curly Braces
+// [] = Brackets/Square brackets
+
+const myArr = [0, 1, 2, 3, 4, 5]
+// The things inside of an Array are called "ELEMENTS". ex - 1.
+
+console.log(myArr[0]);
