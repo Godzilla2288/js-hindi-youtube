a6cdca2b1f68b69976345383155734419ab2e15e
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-03-11 14:09:32 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -7,4 +7,16 @@
 const myArr = [0, 1, 2, 3, 4, 5]
 // The things inside of an Array are called "ELEMENTS". ex - 1.
 
-console.log(myArr[0]);
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
+
+// Array methods
+
+myArr.push(6) // It simply ADDS a value to an Array.
+myArr.push(7)
+myArr.pop()  // It simply REMOVES the Last value from an Array.
+console.log(myArr);
+
+
