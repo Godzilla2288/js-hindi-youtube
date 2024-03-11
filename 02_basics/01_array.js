ef2b3cda7f5b8654632f95ef0bed2b06f6f4f0aa
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-03-11 18:11:43 +0000

    pops

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -14,9 +14,23 @@ console.log(myArr[1]);
 
 // Array methods
 
-myArr.push(6) // It simply ADDS a value to an Array.
-myArr.push(7)
-myArr.pop()  // It simply REMOVES the Last value from an Array.
+// myArr.push(6) // It simply ADDS a value to an Array.
+// myArr.push(7)
+// myArr.pop()  // It simply REMOVES the Last value from an Array.
+
+// myArr.unshift(9) // Adds a value to the FRONT of an Array.
+// myArr.unshift(10) // => [ 10, 9, 0, 1, 2, 3, 4, 5]
+
+// myArr.shift()  // Deletes a value from the FRONT of an Array.
+
+// console.log(myArr.includes(9)); // Asks the question of whether the Array includes
+//             // 9 or not?? [ In this case it returns => false (Boolean Type)]
+// console.log(myArr.indexOf(9));  // => -1 . In this 9 does not Exist in the Array,          
+//             // and so it returns -1 .
+// console.log(myArr.indexOf(3));  // => 3 . In this case no 3 Exist in the Array,
+//             // and so it returns the Index Position of no 3 which is 3 in this case.
+            
+            
 console.log(myArr);
 
 
