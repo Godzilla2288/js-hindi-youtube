80d383f73aba548dcb917ea14774252ee545a7d0
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-03-23 17:45:18 +0000

    object

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,46 @@
+
+
+// const tinderuser = new Object() => {} // Singleton Object
+const tinderuser = {} // => {}  // Non Singleton Object
+
+tinderuser.id = "123abc"
+tinderuser.name = "Sammy"
+tinderuser.isLoggedIn = false
+  // =>  { id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+// console.log(tinderuser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "sourav",
+            lastname: "guha roy"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {2: "c", 4: "d"}
+const obj4 = {5: "e", 5: "f"}
+
+// const obj3 = { obj1, obj2}
+
+// const obj3 = Object.assign(obj1, obj2,obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+console.log(obj3);
+
+
+
+
+
+
+
+
+
+
+
