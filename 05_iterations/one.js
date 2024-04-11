592a089ad122372e974485704c17cd8ffc612b44
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-04-11 07:04:04 +0000

    name1

---
+++ 05_iterations/one.js
@@ -0,0 +1,31 @@
+// Terminal - node 05_iterations/one.js
+
+//    For   Loop
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+}
+
+// console.log(index);  //  => ReferenceError: array is not defined
+// Block Scope. 'index' Variable can not be accessed Outside the For Loop.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
