05669926e8345e9f5dc46aed99f3b2042285af0b
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-04-11 14:24:20 +0000

    while

---
+++ 05_iterations/two.js
@@ -0,0 +1,36 @@
+// Terminal - node 05_iterations/two.js
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['flash', 'batman', 'superman']
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Superhero is ${myArray[arr]}`);
+    arr = arr + 1    // or, arr++. Same output and the same thing but Code Readability should be 
+}                                                               // the Topmost Priority.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
