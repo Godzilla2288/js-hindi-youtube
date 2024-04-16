692a75a662e762f2108f84c75e3c0a11a81735e0
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-04-16 09:13:50 +0000

    for loop

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,4 +1,4 @@
-// Terminal - 
+// Terminal - node 05_iterations/three.js
 
 //  for of
 
@@ -37,9 +37,112 @@ const map = new Map()
 map.set('IN', "India")                      // key value pair
 map.set('USA', "United States of America")  // You get all "unique" values in Maps
 map.set('Fr', "France") // It remembers the original insertion order of the keys, unlike objects
-map.set('IN', "India")                      // key value pair
+map.set('IN', "India")  // It doesn't get printed as Maps are known for Unique Values, there
+                       //  can not be two same values.
+// console.log(map);
 
-console.log(map);
+// =>
+// Map(3) {
+//     'IN' => 'India',
+//     'USA' => 'United States of America',
+//     'Fr' => 'France'
+//   }
+
+for (const key of map) {
+    console.log(key);
+}
+
+// => 
+// [ 'IN', 'India' ]
+// [ 'USA', 'United States of America' ]
+// [ 'Fr', 'France' ]
+
+for (const [key, value] of map) {               //  Array de-structure.
+    // console.log(key, ':-', value);              //  If you don't want array as output then use
+}                                               //  this method.
+
+// =>
+// IN :- India
+// USA :- United States of America
+// Fr :- France
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);            // =>   TypeError: myObject is not iterable      
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
