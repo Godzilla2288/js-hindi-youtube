af0bc19aec13d62c41d130978c38dfc397483b4d
Godzilla2288 <144841632+Godzilla2288@users.noreply.github.com>
2024-04-17 11:21:56 +0000

    map

---
+++ 05_iterations/seven.js
@@ -0,0 +1,61 @@
+// Terminal - node 05_iterations/seven.js
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+                                                           // Same Call-Back Function like Before
+ 
+const newNums = myNumbers.map( (num) => num + 10 )          // No need to use 'Return' Keyword.
+// const newNums = myNumbers.map( (num) => { return num + 10 } )
+
+console.log(newNums);                                                // Automatically  Returns.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -46,12 +46,39 @@ console.log(newNums2);
 
 // => [ 5, 6, 7, 8, 9, 10 ]
 
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
 
+const userBooks = books.filter( (bk) => bk.edition > 2010 )
+// const userBooks = books.filter( (bk) => {                                 // Here used 'Return'
+//     return bk.genre === 'Science' && bk.edition > 2014                   // Keyword because of
+//   } )                                                                   // Curly Braces.[Scope]
+console.log(userBooks);
 
-
-
-
-
+//  =>
+// [
+//     {
+//       title: 'Book Five',
+//       genre: 'Science',
+//       publish: 2009,
+//       edition: 2014
+//     },
+//     {
+//       title: 'Book Eight',
+//       genre: 'Science',
+//       publish: 2011,
+//       edition: 2016
+//     }
+//   ]
 
 
 
